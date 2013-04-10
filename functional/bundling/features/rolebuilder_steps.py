0d0144a26d8ac2a8a379c8742a12956ca9fbf1c7
gigimon <gigimon4ik@gmail.com>
2013-04-10 15:48:50 +0300

    * fix rolebuilder test

--- functional/bundling/features/rolebuilder_steps.py
+++ functional/bundling/features/rolebuilder_steps.py
@@ -17,25 +17,37 @@ LOG = logging.getLogger('rolebuilder')
 
 @step('I start build role')
 def start_rolebuild(step):
-    location = 'us-east-1'
-    if CONF.main.platform == 'rackspace':
-        location = 'rs-ORD1'
-    bundle_id = IMPL.role_build(platform=CONF.main.platform,
-                                                            location=location,
-                                architecture=CONF.main.arch,
-                                                            behaviors=CONF.main.behaviors,
-                                os=CONF.main.dist,
-                                role_name='tmp-%s-%s-%s' % (CONF.main.platform, CONF.main.dist,
+    location = CONF.platforms[CONF.main.platform]['location']
+    if CONF.main.platform == 'rackspaceng':
+        platform = 'rackspacengus'
+    else:
+        platform = CONF.main.platform
+    os_family, os_version = re.findall(r'([a-zA-Z]+)(\d+)', CONF.main.dist)[0]
+    if os_family == 'centos':
+        images = IMPL.rolebuilder.images2()[CONF.main.platform]['images']
+        for image in images:
+            if image['os_family'] == os_family and image['os_version'].startswith(os_version) and \
+                image['architecture'] == CONF.main.arch and image['cloud_location'] == location and not 'hvm' in image:
+                os_version = image['os_version']
+                break
+    else:
+        os_version = os_version[:2] + '.' + os_version[2:]
+    bundle_id = IMPL.rolebuilder.build2(platform=platform,
+                                        location=location,
+                                        arch=CONF.main.arch,
+                                        behaviors=CONF.main.behaviors,
+                                        os_family=os_family,
+                                        os_version=os_version,
+                                        name='tmp-%s-%s-%s' % (CONF.main.platform, CONF.main.dist,
                                                                datetime.now().strftime('%m%d')),
-                                scalarizr=CONF.main.branch,
-                                                    )
+                                        scalarizr=CONF.main.branch,)
     setattr(world, 'role_type', CONF.main.behaviors[0])
     setattr(world, 'bundle_id', bundle_id)
 
 
 @step('Build task started')
 def assert_build_started(step):
-    logs = IMPL.bundletask_get_logs(world.bundle_id)
+    logs = IMPL.bundle.logs(world.bundle_id)
     for l in logs:
         if 'Bundle task created' in l['message']:
             return True
@@ -50,7 +62,7 @@ def assert_build_started(step):
 
 @step('I have new role id')
 def assert_build_started(step):
-    logs = IMPL.bundletask_get_logs(world.bundle_id)
+    logs = IMPL.IMPL.bundle.logs(world.bundle_id)
     for l in logs:
         if 'Role ID:' in l['message']:
             world.new_role_id = re.findall(r"Role ID: ([\d]+)", l['message'])[0]
