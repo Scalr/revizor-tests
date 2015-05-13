b93df4410f04f82c7fc8a6dabc5eb42519de5913
Eugeny Kurkovich <stunko@scalr.com>
2015-05-13 16:09:25 +0300

    REV-67 added waiting client.pem modification

--- functional/lifecycle/steps/chef_boot_steps.py
+++ functional/lifecycle/steps/chef_boot_steps.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from lettuce import world, step
 
@@ -45,18 +46,24 @@ def verify_chef_log(step, serv_as, text):
             return
 
 
-@step("I ([\w\d]+) chef bootstrap stat on ([\w\d]+)")
-def step_impl(step, asction, serv_as):
+@step("I (?:[\w\d]+) chef bootstrap stat on ([\w\d]+)")
+def step_impl(step, serv_as):
+
     server = getattr(world, serv_as)
     node = world.cloud.get_node(server)
-    # Get chef client.pem update time
-    bootstrap_stat = node.run('stat -c %Y /etc/chef/client.pem')[0].split()[0]
-    LOG.debug('Chef client.pem, last modification time: %s' % bootstrap_stat)
-    if asction.strip() == 'save':
-        setattr(world, '%s_bootstrap_stat' % server.id, bootstrap_stat)
-        return
-    saved_bootstrap_stat = getattr(world, '%s_bootstrap_stat' % server.id)
-    LOG.debug('First time chef client.pem, last modification stat: %s' % bootstrap_stat)
-    assert bootstrap_stat > saved_bootstrap_stat, \
-        'Node was not bootstrap with chef after resume.'
+    saved_bootstrap_stat = getattr(world, '%s_bootstrap_stat' % server.id, None)
+
+    for _ in range(10):
+        # Get chef client.pem update time
+        bootstrap_stat = node.run('stat -c %Y /etc/chef/client.pem')[0].split()[0]
+        LOG.debug('Chef client.pem, last modification time: %s' % bootstrap_stat)
+        if not saved_bootstrap_stat:
+            LOG.debug('Save chef client.pem, last modification time: %s' % bootstrap_stat)
+            setattr(world, '%s_bootstrap_stat' % server.id, bootstrap_stat)
+            break
+        elif bootstrap_stat > saved_bootstrap_stat:
+            break
+        time.sleep(30)
+    else:
+        raise AssertionError('Chef client.pem, was not modified after resume.')
 
