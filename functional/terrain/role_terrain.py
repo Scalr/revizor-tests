b19db1a86ae3de0200cb707733be641e2f402c2f
Eugeny Kurkovich <stunko@scalr.com>
2015-06-10 19:21:21 +0300

    *Fix VPC in add_new_role_to_farm created by bundle task

--- functional/terrain/role_terrain.py
+++ functional/terrain/role_terrain.py
@@ -75,19 +75,21 @@ def assert_bundletask_completed(step, serv_as, timeout=1800):
 @step('I add to farm role created by last bundle task(?: as ([\w\d]+) role)?')
 def add_new_role_to_farm(step, alias=None):
     #TODO: Add support for HVM (and VPC)
+    # VPC Support
+    use_vpc = (CONF.feature.use_vpc and CONF.feature.driver.is_platform_ec2) or \
+              (CONF.feature.dist in ('rhel7', 'amzn1503') and CONF.feature.driver.is_platform_ec2)
+
     LOG.info('Add rebundled role to farm with alias: %s' % alias)
-    use_vpc = False
+
     options = getattr(world, 'role_options', {})
     scripting = getattr(world, 'role_scripting', [])
+
     bundled_role = Role.get(world.bundled_role_id)
     alias = alias or bundled_role.name
     if 'redis' in bundled_role.behaviors:
         options.update({'db.msr.redis.persistence_type': os.environ.get('RV_REDIS_SNAPSHOTTING', 'aof'),
                         'db.msr.redis.use_password': True})
-    if CONF.feature.use_vpc \
-            and CONF.feature.dist in ('ubuntu1404', 'rhel7', 'amzn1409') \
-            and CONF.feature.driver.scalr_cloud == 'ec2':
-        use_vpc = True
+
     world.farm.add_role(world.bundled_role_id, options=options,
                         scripting=scripting, alias=alias, use_vpc=use_vpc)
     world.farm.roles.reload()
