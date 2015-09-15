9e2a8ac213d1536951fdc12e90c56dada7ae881d
Eugeny Kurkovich <stunko@scalr.com>
2015-09-15 14:13:15 +0300

    *Fix scalr_api v.2 send request

--- functional/scalrapi/steps/steps.py
+++ functional/scalrapi/steps/steps.py
@@ -36,56 +36,48 @@ CONST = namedtuple(
             1
     )
 
-def send_request(key, uri, params=None, body=None):
-    secretKey = key.get('secretKey')
-    keyId = key.get('keyId')
-    jsonEncodedBody = json.dumps(body) if body else ''
-    # date_format
-    # /2015-02-17T13:54:06Z
+def send_request(method, uri, secret_key, key_id, params=None, body=None):
+    # Set body
+    body = json.dumps(body, separators=(',',':')) if body else ''
+    LOG.debug('Body: %s' % body)
+
+    # Set date, format 2015-02-17T13:54:06Z
     sig_date = time.strftime(CONST.api_date_fmt, time.gmtime())
     LOG.debug('Sign date: %s' % sig_date)
+
     # Set string to sign
-    canonicalQueryString = ''
-    if (params):
-        canonicalQueryString = urlencode(
-            sorted(params.items(), key=lambda x: x[0])
-        )
-    stringToSign = '\n'.join(('GET', sig_date, uri, canonicalQueryString, jsonEncodedBody))
-    LOG.debug('String to sign: %s' % ''.join(stringToSign.split()))
+    query_string = urlencode(sorted(params.items())) if params else ''
+    LOG.debug('query_string: %s' % query_string)
+
+    string_to_sign = '\n'.join((method.upper(), sig_date, uri, query_string, body))
+    LOG.debug('String to sign: %s' % ''.join(string_to_sign))
+
     # Sign request
-    digest = hmac.new(str(secretKey), str(stringToSign), hashlib.sha256).digest()
-    sig = binascii.b2a_base64(digest)
-    if sig.endswith('\n'):
-        sig = sig[:-1]
-    LOG.debug('X-Scalr-Signature : %s' % sig)
+    digest = hmac.new(str(secret_key), str(string_to_sign), hashlib.sha256).digest()
+    sig = binascii.b2a_base64(digest).strip()
+    LOG.debug('X-Scalr-Signature: %s' % sig)
     # Set headers
-    headers = {
-        'Content-Type': 'application/json; charset=utf-8',
-        'X-Scalr-Key-Id': str(keyId),
-        'X-Scalr-Date': sig_date,
-        'X-Scalr-Signature': ' '.join(('V1-HMAC-SHA256', sig)),
-        'X-Scalr-Debug': CONST.api_debug
-    }
+    headers = dict()
+    headers['Content-Type'] =  'application/json; charset=utf-8'
+    headers['X-Scalr-Key-Id'] = str(key_id)
+    headers['X-Scalr-Date'] = sig_date
+    headers['X-Scalr-Signature'] =  '%s %s' % ('V1-HMAC-SHA256', sig)
+    headers['X-Scalr-Debug'] =  CONST.api_debug
+    LOG.debug('Header : %s' % headers)
+
     # Set endpoint
-    endpoint_args = (
-        CONST.api_scheme,
-        CONST.api_host,
-        uri,
-        '', '', ''
-    )
-    endpoint = urlunparse(data=endpoint_args)
-    LOG.debug('Endpoint : %s Header : %s' % (endpoint, headers))
+    endpoint = urlunparse((CONST.api_scheme, CONST.api_host, uri, '', query_string, ''))
+    LOG.debug('Endpoint : %s' % endpoint)
 
     try:
-        resp = requests.get(endpoint, params=params, headers=headers)
+        resp = requests.request(method.lower(), endpoint, data=body, headers=headers)
         resp.raise_for_status()
     except requests.HTTPError as e:
-        LOG.error('An error occurred: Url: {url} Message: {message} Code: {code}'.format(
+        LOG.derror('An error occurred: Url: {url} Message: {message} Code: {code}'.format(
             url = resp.url,
-            **resp.json().get('errors', [{'message': '', 'code': ''}])[0])
-        )
+            **resp.json().get('errors', [{'message': '', 'code': ''}])[0]
+        ))
     return resp
-
 
 @step(r'I have ([\d]+) new api secret key(?:s)?')
 def generate_new_keys(step, count):
@@ -101,7 +93,9 @@ def generate_new_keys(step, count):
 @step(r'I generate (more\sthan\s)?([\d]+) api queries for one minute(\susing second secret key)?')
 def send_api_requests(step, inc_count, queries_count, key_id):
     def task():
-        resp = send_request(key, uri, dict(maxResults=2))
+        secret_key = key.get('secretKey')
+        key_id = key.get('keyId')
+        resp = send_request('get', uri, secret_key, key_id, params=dict(maxResults=2))
         return resp.status_code
 
     key = getattr(world, 'keys')[bool(key_id)]
@@ -137,7 +131,9 @@ def send_failed_request(step):
     key = getattr(world, 'keys')[0]
     scalr_env = getattr(IMPL, 'account').get_env()
     uri = CONST.api_path.format(envid=scalr_env['env_id'], api_method='failed')
-    resp = send_request(key, uri, dict(maxResults=2))
+    secret_key = key.get('secretKey')
+    key_id = key.get('keyId')
+    resp = send_request('get', uri, secret_key, key_id, params=dict(maxResults=2))
     world.tasks_results = resp.status_code
     LOG.debug('Failed result: %s' % resp.json())
 
