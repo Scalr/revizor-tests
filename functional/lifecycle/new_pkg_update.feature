5d09e0cf1a15dea40fbd1ec6507ffb55b1fb3ae4
Eugeny Kurkovich <stunko@scalr.com>
2015-09-21 15:41:37 +0300

    REV-3 Refactoring linux test

--- functional/lifecycle/new_pkg_update.feature
+++ functional/lifecycle/new_pkg_update.feature
@@ -1,39 +1,53 @@
-Using step definitions from: steps/new_pkg_update_steps, steps/import_steps
+Using step definitions from: steps/new_pkg_update_steps, steps/import_steps, steps/lifecycle_steps
 Feature: Update scalarizr test
 
-    @ec2 @gce @cloudstack @rackspaceng @openstack
-    Scenario: Update at bootstrap, new role create
-      Given I have a server running in cloud
-      When I install scalarizr to the server
-      Then I create image
+    @linux @ec2 @gce @cloudstack @rackspaceng @openstack
+    Scenario: Update at bootstrap linux test, new role create
+      Given I have a clean image
       And I add image to the new role
 
-    @ec2 @gce @cloudstack @rackspaceng @openstack
-    Scenario: Update at bootstrap, use new role
+    @linux @ec2 @gce @cloudstack @rackspaceng @openstack
+    Scenario: Update at bootstrap  linux test, use new role
       Given I have a an empty running farm
       And I add created role to the farm
-      When I expect server bootstrapping as M1
-      Then scalarizr version is valid in M1
-      And update-client version is valid in M1
-      When I execute script 'Test script name' synchronous on M1
-      And I see script result in M1
+      When I see pending server M1
+      Then I install scalarizr to the server M1
+      And I reboot server M1
+      When I expect server bootstrapping as M2
+      Then scalarizr version is valid in M2
+      When I execute script 'Test script name' synchronous on M2
+      And I see script result in M2
+
+    @win @ec2 @gce @cloudstack @rackspaceng @openstack
+    Scenario: Update at bootstrap windows test, new role create
+      Given I have a server running in cloud
+      When I install scalarizr to the server
+      Then I create image from deployed server
+      And I add image to the new role
+
+    @win @ec2 @gce @cloudstack @rackspaceng @openstack
+    Scenario: Update at bootstrap windows test, use new role
+      Given I have a an empty running farm
+      And I add created role to the farm
+      When I expect server bootstrapping as M2
+      Then scalarizr version is valid in M2
+      When I execute script 'Test script name' synchronous on M2
+      And I see script result in M2
 
     @ec2 @gce @cloudstack @rackspaceng @openstack
     Scenario: Update by Scalr UI
-      Given I have running server M1
+      Given I have running server M2
       Then I change branch to test-branch for role
       When I trigger scalarizr update by Scalr UI
-      Then scalarizr version is valid in M1
-      Then update-client version is valid in M1
-      Then I execute script 'Test script name' synchronous on M1
-      And I see script result in M1
+      Then scalarizr version is valid in M2
+      Then I execute script 'Test script name' synchronous on M2
+      And I see script result in M2
       Then I fork tested branch to test-branch
       And I change branch to test-branch for role
       When I trigger scalarizr update by Scalr UI
-      Then scalarizr version is valid in M1
-      Then update-client version is valid in M1
-      Then I execute script 'Test script name' synchronous on M1
-      And I see script result in M1
+      Then scalarizr version is valid in M2
+      Then I execute script 'Test script name' synchronous on M2
+      And I see script result in M2
 
 
 
--- functional/lifecycle/steps/new_pkg_update_steps.py
+++ functional/lifecycle/steps/new_pkg_update_steps.py
@@ -5,25 +5,43 @@ Created on 09.01.2015
 @author: Eugeny Kurkovich
 """
 
+import time
 import logging
 
 from datetime import datetime
 from revizor2.api import IMPL
 from revizor2.conf import CONF
-from lettuce import step, world
+from lettuce import step, world, after
 from revizor2.consts import Dist
 from revizor2.defaults import USE_VPC
 from revizor2.helpers import install_behaviors_on_node
+from revizor2.utils import wait_until
 
 
 LOG = logging.getLogger(__name__)
 
-@step(r'I install scalarizr to the server')
-def install_scalarizr(step):
-    cloud_server = getattr(world, 'cloud_server')
+@step(r'I have a clean image')
+def get_clean_image(step):
+    cloud = world.cloud
+    image = cloud.find_image(use_hvm=USE_VPC)
+    LOG.debug('Obtained clean image %s, Id: %s' %(image.name, image.id))
+    setattr(world, 'image', image)
+
+
+@step(r'I install scalarizr to the server(\s[\w\d]+)*$')
+def install_scalarizr(step, serv_as):
+    server = getattr(world, serv_as.strip(), '')
+    node = None
+    # Wait cloud server running
+    if server:
+        cloud = world.cloud
+        server.reload()
+        node = wait_until(cloud.get_node, args=(server, ), timeout=300, logger=LOG)
+        LOG.debug('Node get successfully %s' % node)
+    cloud_server =  node or getattr(world, 'cloud_server', None)
+    assert cloud_server,  'Node not found'
     cookbooks = ['base', 'scalarizr']
     branch = CONF.feature.branch
-
     # Windows handler
     if Dist.is_windows_family(CONF.feature.dist):
         return
@@ -34,12 +52,12 @@ def install_scalarizr(step):
     install_behaviors_on_node(*args, branch=branch)
 
 
-@step(r'I create image')
+@step(r'I create image from deployed server')
 def create_image(step):
     cloud = world.cloud
     cloud_server = getattr(world, 'cloud_server')
-
-    image_name = 'tmp-base-{}-{:%d%m%Y-%H%M}'.format(
+    # Create an image
+    image_name = 'tmp-base-{}-{:%d%m%Y-%H%M%S}'.format(
         CONF.feature.dist,
         datetime.now()
     )
@@ -50,10 +68,9 @@ def create_image(step):
     )
     if CONF.feature.driver.is_platform_ec2:
         kwargs.update({ 'reboot': False})
-
     image = cloud.create_template(**kwargs)
     assert getattr(image, 'id', False), 'An image from a node object %s was not created' % cloud_server.name
-
+    # Remove cloud server
     LOG.info('An image: %s from a node object: %s was created' % (image.id, cloud_server.name))
     setattr(world, 'image', image)
     LOG.debug('Image atrs: %s' % dir(image))
@@ -63,7 +80,6 @@ def create_image(step):
         ip = world.ip
         assert cloud.close_port(cloud_server, forwarded_port, ip=ip), "Can't delete a port forwarding rule."
     LOG.info('Port forwarding rule was successfully removed.')
-
     assert cloud_server.destroy(), "Can't destroy node: %s." % cloud_server.id
     LOG.info('Virtual machine %s was successfully destroyed.' % cloud_server.id)
     setattr(world, 'cloud_server', None)
@@ -71,6 +87,7 @@ def create_image(step):
 
 @step(r'I add image to the new role')
 def create_role(step):
+    image_exists = False
     cloud_location = CONF.platforms[CONF.feature.platform]['location'] \
         if not CONF.feature.driver.is_platform_gce else ""
     image_kwargs = dict(
@@ -78,19 +95,23 @@ def create_role(step):
         cloud_location =  cloud_location,
         image_id = world.image.id
     )
-    name = world.image.name or \
-        'tmp-base-{}-{:%d%m%Y-%H%m}'.format(
+    name = 'tmp-base-{}-{:%d%m%Y-%H%M%S}'.format(
             CONF.feature.dist,
-            datetime.now()
-        )
+            datetime.now())
     behaviors = ['bas','chef']
     # Checking an image
-    LOG.debug('Checking an image {image_id}:{platform}({cloud_location})'.format(**image_kwargs))
-    IMPL.image.check(**image_kwargs)
-    # Register image to the Scalr
-    LOG.debug('Register image %s to the Scalr' % name)
-    image_kwargs.update(dict(software=behaviors, name=name))
-    IMPL.image.create(**image_kwargs)
+    try:
+        LOG.debug('Checking an image {image_id}:{platform}({cloud_location})'.format(**image_kwargs))
+        IMPL.image.check(**image_kwargs)
+    except Exception as e:
+        if not ('Image has already been registered' in e.message):
+            raise
+        image_exists = True
+    if not image_exists:
+        # Register image to the Scalr
+        LOG.debug('Register image %s to the Scalr' % name)
+        image_kwargs.update(dict(software=behaviors, name=name))
+        IMPL.image.create(**image_kwargs)
     # Create new role
     role_kwargs = dict(
         name =  name,
@@ -115,12 +136,14 @@ def setup_farm(step):
             "base.upd.repository": branch,
             "base.devel_repository": CONF.feature.ci_repo,
             "user-data.scm_branch": branch},
+        alias = world.role['name'],
         use_vpc = USE_VPC
     )
     farm.add_role(world.role['id'], **role_kwargs)
     farm.roles.reload()
     farm_role = farm.roles[0]
     LOG.info('Change branch to: %s for role: %s' % (branch, farm_role.id))
+    setattr(world, '%s_role' % world.role['name'], farm_role)
 
 
 @step(r'I trigger scalarizr update by Scalr UI')
@@ -136,4 +159,9 @@ def assert_version(step, service, serv_as):
 
 @step(r'I fork ([A-za-z0-9\/\-\_]+) branch to ([A-za-z0-9\/\-\_]+)$')
 def fark_git_branch(step, branch_from, branch_to):
+    pass
+
+
+@after.all
+def cleanup(total):
     pass
