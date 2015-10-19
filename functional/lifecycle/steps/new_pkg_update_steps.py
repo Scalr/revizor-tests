ce46d3bbf199d9581712850f922ed7c3a95085dc
Theramas <n.kalmikov@scalr.com>
2015-10-19 17:15:22 +0300

    Add second cmd run for scalarizr version

--- functional/lifecycle/steps/new_pkg_update_steps.py
+++ functional/lifecycle/steps/new_pkg_update_steps.py
@@ -87,11 +87,11 @@ def installing_scalarizr(step, serv_as=''):
             username = 'scalr'
         console = winrm.Session('http://%s:5985/wsman' % node.public_ips[0],
                                 auth=(username, 'scalr'))
-        command = '''powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://my.scalr.net/public/windows/stridercd/%s/install_scalarizr.ps1'))" ; scalarizr -v''' % CONF.feature.branch
-        print command
+        command = '''powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://my.scalr.net/public/windows/stridercd/%s/install_scalarizr.ps1'))"''' % CONF.feature.branch
         out = console.run_cmd(command)
         print out.std_err
-        res = out.std_out
+        res = console.run_cmd('scalarizr -v').std_out
+        print res
         run_sysprep(node, console)
     # Linux handler
     else:
