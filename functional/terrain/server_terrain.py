e718760c077efb2a372a96339e279a254462e3ea
gigimon <gigimon4ik@gmail.com>
2015-10-22 19:01:01 +0300

    * fix previous commit from Nikolay Kalmikov

--- functional/terrain/server_terrain.py
+++ functional/terrain/server_terrain.py
@@ -230,7 +230,7 @@ def verify_string_in_file(step, file_path, value, serv_as):
 
 
 @step(r'I have a ([\w\d]+) attached volume as ([\w\d]+)')
-@world.run_only_if(storage='persistent', platform='!'+Platform.EC2)
+@world.run_only_if(storage='persistent')
 def save_attached_volume_id(step, serv_as, volume_as):
     server = getattr(world, serv_as)
     attached_volume = None
@@ -268,7 +268,7 @@ def save_attached_volume_id(step, serv_as, volume_as):
 
 
 @step(r'attached volume ([\w\d]+) has size (\d+) Gb')
-@world.run_only_if(platform=(Platform.CLOUDSTACK), storage='persistent')
+@world.run_only_if(storage='persistent')
 def verify_attached_volume_size(step, volume_as, size):
     LOG.info('Verify attached volume has new size "%s"' % size)
     size = int(size)
