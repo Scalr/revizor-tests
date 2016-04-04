55e5363a711d8f02fb9f406c7fe4d7315b6c9f21
Eugeny Kurkovich <stunko@scalr.com>
2016-04-04 18:01:45 +0300

    REV-143 *fix comments

--- functional/lifecycle/steps/lifecycle_steps.py
+++ functional/lifecycle/steps/lifecycle_steps.py
@@ -333,4 +333,4 @@ def checking_info_instance_vcpus(step, serv_as):
     server = getattr(world, serv_as)
     vcpus = int(server.details()['info.instance_vcpus'])
     LOG.info('Server %s vcpus info: %s' % (server.id, vcpus))
-    assert vcpus > 0, 'info.instance_vcpus: %s not valid for %s' % (vcpus, server.id)
+    assert vcpus > 0, 'info.instance_vcpus not valid for %s' % server.id
