753141f8ca5ffea909fbcccbc9a1af67020ddb24
gigimon <gigimon4ik@gmail.com>
2016-09-21 14:56:07 +0300

    * mayve fix backup

--- functional/databases/steps/common_steps.py
+++ functional/databases/steps/common_steps.py
@@ -261,11 +261,13 @@ def assert_check_databundle_date(step, back_type):
     if CONF.feature.driver.current_cloud in [Platform.CLOUDSTACK, Platform.IDCF, Platform.KTUCLOUD]:
         LOG.info('Platform is cloudstack-family, backup not doing')
         return True
-    info = world.get_role().db.info()
-    if info['last_%s' % back_type] == 'In progress...':
-        while world.get_role().db.info()['last_%s' % back_type] == 'In progress...':
-            LOG.debug('Last %s in progress, wait 10 seconds' % back_type)
-            time.sleep(10)
+    for attempt in range(6):
+        LOG.debug('%s attempt to check databnundle status' % attempt)
+        info = world.get_role().db.info()
+        LOG.info('%s now in %s state' % (back_type, info['last_%s' % back_type]))
+        if info['last_%s' % back_type] not in ['In progress...', 'Never']:
+            break
+        time.sleep(10)
     if not info['last_%s' % back_type] == getattr(world, 'last_%s' % back_type, 'Never'):
         return
     else:
