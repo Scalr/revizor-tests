a4cda13a33f54d1570e040d1b436ae753047b42b
Nazar Ner <n.ner@scalr.com>
2017-07-04 15:31:28 +0300

    Changes: attemt

--- functional/lifecycle/steps/lifecycle_steps.py
+++ functional/lifecycle/steps/lifecycle_steps.py
@@ -158,9 +158,9 @@ def execute_command(step, command, serv_as):
 @step('server ([\w\d]+) contain \'(.+)\'')
 def check_file(step, serv_as, path):
     node = world.cloud.get_node(getattr(world, serv_as))
-    for out in range(5):
+    for _ in range(5):
         out = node.run('ls %s' % path)
-        LOG.info('Check exist path: %s' % path)
+        LOG.info('Check exist path: %s. Attempt: %s' % path)
         if out[2] == 0: # success
             break
         time.sleep(15)
