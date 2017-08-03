d1a93a2b21716720db477091b094c52c5c5899c1
Eugeny Kurkovich <stunko@users.noreply.github.com>
2017-08-03 20:52:22 +0300

    REV-317: Fix unstored messages polling (#106)

--- functional/terrain/libs/server.py
+++ functional/terrain/libs/server.py
@@ -387,6 +387,38 @@ def wait_unstored_message(server, message_name, message_type='out', timeout=600)
 
 
 @world.absorb
+def wait_unstored_message(server, message_name, message_type='out', timeout=600):
+    node = world.cloud.get_node(server)
+    lookup_messages = getattr(world, '_server_%s_lookup_messages' % server.id, [])
+    message_type = 'in' if message_type.strip() in ('sends', 'out') else 'out'
+    LOG.info('Searching message "%s/%s" on %s node' % (message_type, message_name, server.id))
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        list_messages = reversed(world.get_szr_messages(node, convert=True))
+        messages = filter(lambda m: m.name == message_name and m.direction == message_type, list_messages)
+        for message in messages:
+            if message.id in lookup_messages:
+                LOG.info('Message already lookuped %s' % message.id)
+                continue
+            elif strtobool(message.handled):
+                LOG.info('Message found: %s' % message.id)
+                lookup_messages.append(message.id)
+                setattr(world,
+                        '_server_%s_lookup_messages' % server.id,
+                        lookup_messages)
+                return server
+            else:
+                LOG.info('Message not handled: %s' % message.id)
+                break
+        time.sleep(30)
+    else:
+        raise MessageNotFounded('%s/%s was not finding on %s node' % (
+            message_type,
+            message_name,
+            server.id))
+
+
+@world.absorb
 def wait_server_message(server, message_name, message_type='out', find_in_all=False, timeout=600):
     """
     Wait message in server list (or one server). If find_in_all is True, wait this message in all
