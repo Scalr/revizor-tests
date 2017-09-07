ed3fb10afcc40974e0bcb879aa38f056a842515c
Theramas <n.kalmikov@scalr.com>
2017-09-07 13:31:10 +0300

    REV-247 Exclude unsupported orchestration and chef-related steps by hooks

--- functional/terrain/hooks_terrain.py
+++ functional/terrain/hooks_terrain.py
@@ -30,6 +30,62 @@ LOG = logging.getLogger(__name__)
 
 OUTLINE_ITERATOR = {}
 PKG_UPDATE_SUITES = ['Linux update for new package test', 'Windows update for new package test']
+COREOS_UNSUPPORTED_SCRIPTS = [
+  {
+    'user': 'root',
+    'exitcode': '0',
+    'stdout': '"HOME"=>"/root"; "USER"=>"root"',
+    'event': 'BeforeHostUp',
+    'name': 'chef'
+  },
+  {
+    'user': 'root',
+    'exitcode': '0',
+    'stdout': '',
+    'event': 'HostInit',
+    'name': 'Revizor orchestration init'
+  },
+  {
+    'user': 'revizor2',
+    'exitcode': '1',
+    'stdout': '',
+    'event': 'HostUp',
+    'name': 'Linux ping-pong'
+  },
+  {
+    'user': 'revizor',
+    'exitcode': '0',
+    'stdout': 'Local script work! User: revizor; USER=revizor; HOME=/home/revizor',
+    'event': 'HostUp',
+    'name': '/home/revizor/local_script.sh'
+  },
+  {
+    'user': 'revizor',
+    'exitcode': '0',
+    'stdout': 'pong',
+    'event': 'HostUp',
+    'name': 'Linux ping-pong'
+  },
+  {
+    'user': 'root',
+    'exitcode': '0',
+    'stdout': '',
+    'event': 'HostUp',
+    'name': 'chef'
+  },
+    {
+      'output': 'Local script work!; USER=root; HOME=/root',
+      'execute_type': 'synchronous',
+      'script_name': '/home/revizor/local_script.sh',
+      'script_type': 'local'
+    },
+    {
+      'output': 'Local script work!; USER=root; HOME=/root',
+      'execute_type': 'asynchronous',
+      'script_name': '/home/revizor/local_script.sh',
+      'script_type': 'local'
+    }
+]
 
 ORG = 'Scalr'
 GH = github.GitHub(access_token=CONF.credentials.github.access_token)
@@ -146,7 +202,7 @@ def get_scalaraizr_latest_version(branch):
         default_repo = url.format(path=path)
     index_url = default_repo.format(branch=branch)
     repo_data = parser_for_os_family(CONF.feature.dist.mask)(branch=branch, index_url=index_url)
-    versions = [package['version'] for package in repo_data if package['name'] == 'scalarizr']
+    versions = [package['version'] for package in repo_data if package['name'] == 'scalarizr'] if os_family != 'coreos' else repo_data
     versions.sort(reverse=True)
     return versions[0]
 
@@ -227,7 +283,10 @@ def exclude_steps_by_options(feature):
     for scenario in feature.scenarios:
         steps_to_remove = set()
         for step in scenario.steps:
-            func = step._get_match(None)[1].function
+            custom_sentence = None
+            if scenario.outlines:
+                custom_sentence = re.sub('<.+>', scenario.outlines[0].values()[0], step.sentence)
+            func = step._get_match(None, custom_sentence=custom_sentence)[1].function
             if hasattr(func, '_exclude'):
                 steps_to_remove.add(step)
         for step in steps_to_remove:
@@ -301,6 +360,12 @@ def exclude_update_from_latest(feature):
             scenario = [s for s in feature.scenarios if s.name == 'Update from latest to branch from ScalrUI'][0]
             feature.scenarios.remove(scenario)
             LOG.info("Removed scenario: %s" % scenario)
+
+@before.each_feature
+def exclude_chef_orchestration_for_coreos(feature):
+    if feature.name == 'Orchestration features test' and CONF.feature.dist.id == 'coreos':
+        for scenario in feature.scenarios:
+            scenario.outlines = [o for o in scenario.outlines if o not in COREOS_UNSUPPORTED_SCRIPTS]
 
 
 @after.outline
