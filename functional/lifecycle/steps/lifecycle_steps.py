52d381220c3c609faf03b5a32c97f61edc55e4f4
Nazar Ner <n.ner@scalr.com>
2017-10-31 20:19:27 +0200

    Added before each_scenario for remove raid support

--- functional/lifecycle/steps/lifecycle_steps.py
+++ functional/lifecycle/steps/lifecycle_steps.py
@@ -5,7 +5,7 @@ import json
 import logging
 from itertools import chain
 
-from lettuce import world, step, after
+from lettuce import world, step, after, before
 
 from revizor2.api import IMPL
 from revizor2.conf import CONF
@@ -408,3 +408,17 @@ def assert_server_message_count(step, msg, serv_as):
     assert messages_count == mount_device_count, (
         'Scalr internal messages count %s != %s Mounted storages count. List of all Incoming msg names: %s ' % (
             messages_count, mount_device_count, incoming_messages))
+
+
+@before.each_scenario
+def remove_raid_support(scenario):
+    if CONF.feature.dist.id != 'centos-6-x' or 'ubuntu-14-04' and CONF.feature.platform != 'ec2':
+        for step in scenario.steps[:]:
+            if step.sentence == "And directory '/media/raidmount' exist in M1":
+                scenario.steps.remove(step)
+            elif step.sentence == "And I create 100 files in '/media/raidmount' in M1":
+                scenario.steps.remove(step)
+            elif step.sentence == "And disk from M1 mount points for '/media/raidmount' exist in fstab on M1":
+                scenario.steps.remove(step)
+            elif step.sentence == "And count of files in directory '/media/raidmount' is 100 in M1":
+                scenario.steps.remove(step)
