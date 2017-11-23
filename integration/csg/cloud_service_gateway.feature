9c5917363e7b1a5820ce8b3f066578a4f767a6d1
Petro Protsakh <p.protsakh@scalr.com>
2017-11-23 14:37:28 +0200

    * csg test: added scenarios for service read access; proxy configuration and csg via proxy access

--- integration/csg/cloud_service_gateway.feature
+++ integration/csg/cloud_service_gateway.feature
@@ -1,20 +1,49 @@
 Feature: Check CSG
-  In order to use CSG feature
-  as a Scalr user
-  I want to be able to manage cloud service access requests
-  and CSG should work properly for AWS and Azure clouds
-  and it works through proxy
+    In order to use CSG feature
+    as a Scalr user
+    I want to be able to manage cloud service access requests
+    and CSG should work properly for AWS and Azure clouds
+    and it works through proxy
 
-  Scenario: Create and approve cloud service access request
-    Given I have requested access to services on AWS as AR1
-      | service         |
-      | Lambda          |
-      | ECS             |
-    And I see access request AR1 in pending status on environment scope
-    And I see access request AR1 in pending status on account scope
-    When I approve access request AR1
-    Then I see access request AR1 in approved status on environment scope
-    And I see access request AR1 in approved status on account scope
-    Then I obtain secret key for access request AR1
-    And I see access request AR1 in active status on environment scope
-    And I see access request AR1 in active status on account scope
+    Scenario: Create and approve cloud service access request
+        Given I have requested access to services on AWS as AR1:
+          | service         |
+          | Lambda          |
+          | ECS             |
+        And I see access request AR1 in pending status on environment scope
+        And I see access request AR1 in pending status on account scope
+        When I approve access request AR1
+        Then I see access request AR1 in approved status on environment scope
+        And I see access request AR1 in approved status on account scope
+        Then I obtain secret key for access request AR1
+        And I see access request AR1 in active status on environment scope
+        And I see access request AR1 in active status on account scope
+
+    Scenario: Check approved services read access
+        Given I have active access request AR1
+        When I execute "list-functions" for Lambda service on AWS using AR1
+        Then the response contains no errors
+
+    Scenario: Check not authorized service access
+        Given I have active access request AR1
+        When I execute "list-reusable-delegation-sets" for Route53 service on AWS using AR1
+        Then the response contains access error
+
+    Scenario: Configure proxy server
+        Given I have configured revizor environment:
+          | name           | value          |
+          | platform       | gce            |
+          | dist           | ubuntu1404     |
+        And I have a clean and stopped farm
+        And I add role to this farm
+        When I start farm
+        Then I expect server bootstrapping as P1
+        And I execute local script 'https://git.io/vFb0z' synchronous on P1
+        And I set proxy for AWS,Azure in Scalr to P1
+        And I restart service "cloud-service-gateway"
+
+    Scenario: Check approved services access via proxy
+        Given I have active access request AR1
+        When I execute "list-functions" for Lambda service on AWS using AR1
+        Then the response contains no errors
+        And last proxy logs on P1 contain "lambda.us-east-1.amazonaws.com"
--- integration/csg/steps/cloud_service_gateway.py
+++ integration/csg/steps/cloud_service_gateway.py
@@ -1,13 +1,16 @@
 import logging
 
 from lettuce import world, step
+
 from revizor2.backend import IMPL
+from revizor2.conf import CONF
+from revizor2.helpers import cloud_services
 
 
 LOG = logging.getLogger(__name__)
 
 
-@step('I have requested access to services on (AWS|Azure) as ([\w\d]+)')
+@step('I have requested access to services on (AWS|Azure) as ([\w\d]+):')
 def request_access(step, cloud, request_as):
     cloud = cloud.lower()
     services = []
@@ -21,9 +24,11 @@ def request_access(step, cloud, request_as):
     setattr(world, '%s_request_cloud' % request_as, cloud)
 
 
-@step('I see access request ([\w\d]+) in ([\w\d]+) status on (environment|account) scope')
+@step('I see access request ([\w\d]+) in ([\w\d]+) status(?: on (environment|account) scope)?')
 def verify_request_status(step, request_as, status, scope):
     id = getattr(world, '%s_request_id' % request_as)
+    if scope is None:
+        scope = 'environment'
     request = IMPL.csg.get_request(id, scope)
     if request['status'].lower() != status.lower():
         raise AssertionError('Cloud service access request status is incorrect. '
@@ -48,3 +53,84 @@ def obtain_key(step, request_as):
         raise AssertionError('Error getting secret key for cloud service access request, id=%s' % id)
     LOG.debug('Obtained secret key for cloud service access request, id=%s' % id)
     setattr(world, '%s_request_secret' % request_as, secret)
+
+
+@step('I have ([\w\d]+) access request ([\w\d]+)')
+def have_request(step, status, request_as):
+    step.given("I see access request {request_as} in {status} status".format(
+        request_as=request_as,
+        status=status))
+
+
+@step('I execute "([\w\d-]+)" for ([\w\d]+) service on (AWS|Azure) using ([\w\d]+)')
+def execute_service_function(step, func, service, cloud, request_as):
+    id = getattr(world, '%s_request_id' % request_as)
+    secret = getattr(world, '%s_request_secret' % request_as)
+    request = IMPL.csg.get_request(id)
+    csg_port = world.get_scalr_config_value('scalr.csg.endpoint.port')
+    result = None
+    if cloud.lower() == 'aws':
+        aws = cloud_services.Aws(id,
+                                 request['access_key'],
+                                 secret,
+                                 'us-east-1',
+                                 '%s.test-env.scalr.com' % CONF.scalr.te_id,
+                                 csg_port)
+        aws.configure()
+        result = aws.execute_function(service.lower(), func.lower())
+    setattr(world, 'csg_last_result', result)
+
+
+@step('the response contains no errors')
+def verify_response_no_error(step):
+    result = getattr(world, 'csg_last_result')
+    if result is None:
+        raise AssertionError('No recent CSG response found')
+    if result['status'] != 200 or result.get('error'):
+        raise AssertionError('Service response is not valid. '
+                             'HTTP status = %s, Error = "%s"' % (result['status'], result.get('error')))
+
+
+@step('the response contains access error')
+def verify_response_access_error(step):
+    result = getattr(world, 'csg_last_result')
+    if result is None:
+        raise AssertionError('No recent CSG response found')
+    if result['status'] not in [400, 401, 403] or not result.get('error'):
+        raise AssertionError('Service response is not valid. '
+                             'HTTP status = %s, Error = %s' % (result['status'], result.get('error')))
+
+
+@step('I set proxy for ([\w\d,]+) in Scalr to ([\w\d]+)')
+def configure_scalr_proxy(step, clouds, proxy_as):
+    clouds = [c.strip().lower() for c in clouds.split(',')]
+    server = getattr(world, proxy_as)
+    params = [
+        {'name': 'scalr.connections.proxy.host', 'value': server.public_ip},
+        {'name': 'scalr.connections.proxy.port', 'value': 3128},
+        {'name': 'scalr.connections.proxy.user', 'value': 'testuser'},
+        {'name': 'scalr.connections.proxy.pass', 'value': 'p@ssw0rd'},
+        {'name': 'scalr.connections.proxy.type', 'value': 0},
+        {'name': 'scalr.connections.proxy.authtype', 'value': 1},
+        {'name': 'scalr.connections.proxy.use_on', 'value': 'scalr'}
+    ]
+    for cloud in clouds:
+        params.append(
+            {'name': 'scalr.%s.use_proxy' % cloud, 'value': True}
+        )
+    world.update_scalr_config(params)
+
+
+@step('last proxy logs on ([\w\d]+) contain "(.+)"')
+def check_proxy_logs(step, proxy_as, contain):
+    server = getattr(world, proxy_as)
+    node = world.cloud.get_node(server)
+    old_logs = getattr(world, '%s_proxy_logs' % proxy_as, [])
+    logs = node.run('cat /var/log/squid3/access.log')[0].splitlines()
+    setattr(world, '%s_proxy_logs' % proxy_as, logs)
+    for line in logs:
+        if contain in line and line not in old_logs:
+            break
+    else:
+        LOG.debug('Received squid logs:\n%s' % logs)
+        raise AssertionError('Text "%s" not found in last proxy logs' % contain)
