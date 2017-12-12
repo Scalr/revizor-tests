3c8c5be96f57b00e15b11549828edccabc77c2c3
Nazar Ner <n.ner@scalr.com>
2017-12-12 13:48:22 +0200

    Added checking attached disks to folder on windows (Not finished)

--- functional/lifecycle/steps/windows_steps.py
+++ functional/lifecycle/steps/windows_steps.py
@@ -73,25 +73,27 @@ def check_errors_in_log(step, serv_as):
         raise AssertionError('ERROR in log: %s' % errors)
 
 
-@step(r"server ([\w\d]+) has disks ([(\w)(\(\w+_label\))?: (\d+) Gb,]+)")
+@step(r"server ([\w\d]+) has disks ([((?:\w):(?:\\\w+)?)(?:\(\w+_label\d*\))? (\d+) Gb,]+)")
 @world.run_only_if(platform=(Platform.EC2, Platform.GCE))
 def check_attached_disk_size(step, serv_as, cdisks):
-    correct_disks = dict(re.findall('(\w)(?:\(\w+_label\))?: (\d+) Gb', cdisks))
+    correct_disks = dict(re.findall('((?:\w):(?:\\\w+)?)(?:\(\w+_label\d*\))? (\d+) Gb', cdisks))
     server = getattr(world, serv_as)
-    out = world.run_cmd_command(server, 'wmic logicaldisk get size,caption,volumename').std_out
-    sdisks = filter(lambda x: x.strip(), out)
-    server_disks = dict(re.findall('(\w):(\d+)(?:\w+_label)?', sdisks))
-    server_disks = {k: int(v)/1000000000 for k, v in server_disks.items()}
+    out = world.run_cmd_command(server, 'wmic volume get Caption,Capacity,Label').std_out
+    sdisks = re.sub('\s+', ' ', out)
+    server_disks = {v.replace('\\\\', '\\').strip('\\'): int(k)/1000000000 for k, v in re.findall('(\d+) ((?:\w):\\\\(?:\w+\\\\)?)', sdisks)}
     for mountpoint in correct_disks:
         if mountpoint in server_disks:
-            assert int(correct_disks[mountpoint]) == server_disks[mountpoint], "Disk %s is of wrong size  - %s " % (mountpoint, server_disks[mountpoint])
+            assert int(correct_disks[mountpoint]) == server_disks[mountpoint],\
+                "Disk %s is of wrong size  - %s " % (mountpoint, server_disks[mountpoint])
         else:
             raise AssertionError("Disk %s is not present" % mountpoint)
-    server_labels = dict(re.findall('(\w):\d+(\w+_label)?', sdisks))
-    correct_labels = dict(re.findall('(\w)(\(\w+_label\))?: \d+', cdisks))
-    for mountpoint in correct_labels:
-        if mountpoint in server_labels:
-            assert correct_labels[mountpoint].strip('()') == server_labels[mountpoint], "Disk %s has an incorrect or no label %s. Should be %s." % (mountpoint,server_labels[mountpoint], correct_labels[mountpoint].strip('()'))
+    # server_labels = dict(re.findall('(\w):\d+(\w+_label)?', sdisks))
+    # correct_labels = dict(re.findall('(\w)(\(\w+_label\))?: \d+', cdisks))
+    # for mountpoint in correct_labels:
+    #     if mountpoint in server_labels:
+    #         assert correct_labels[mountpoint].strip('()') == server_labels[mountpoint],\
+    #             "Disk %s has an incorrect or no label %s. Should be %s." % (
+    #                 mountpoint,server_labels[mountpoint], correct_labels[mountpoint].strip('()'))
 
 
 @step(r"I remove file '([\w\W]+)' from ([\w\d]+) windows")
