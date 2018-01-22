bf53e5a3c983876caa857b7950231ce0ae91bc8a
Petro Protsakh <p.protsakh@scalr.com>
2018-01-22 16:14:51 +0200

    * added Azure > Machine Learning service validation

--- integration/terrain/libs/cloud_services/azure_services/__init__.py
+++ integration/terrain/libs/cloud_services/azure_services/__init__.py
@@ -1,9 +1,10 @@
 import event_hubs
+import machine_learning
 import web
 
 
 services = {
     event_hubs.EventHubs.service_name: event_hubs.EventHubs,
-    web.Web.service_name: web.Web
+    web.Web.service_name: web.Web,
+    machine_learning.MachineLearning.service_name: machine_learning.MachineLearning
 }
-import unittest
