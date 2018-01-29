d138177485e913c3ceef420b8a4ac4691f0ee3bf
Theramas <n.kalmikov@scalr.com>
2018-01-29 12:09:14 +0200

    FIX old code occurrences

--- functional/lifecycle/steps/szradm_steps.py
+++ functional/lifecycle/steps/szradm_steps.py
@@ -280,12 +280,13 @@ def check_variable(step, var, serv_as):
 
     LOG.info('Get variable %s from scalr_globals.sh on %s' % (var, server.id))
     result = node.run("grep '%s' /etc/profile.d/scalr_globals.sh" % var)
-    if len(result[0].split('=')) != 2:
+    if len(result.std_out.split('=')) != 2:
         raise AssertionError("Can't get variable %s from scalr_globals.sh on %s." % (var, server.id))
-    script_result = result[0].split('=')[1].strip('"\n')
+    script_result = result.std_out.split('=')[1].strip('"\n')
     #Get an variable from the environment
     LOG.info('Get variable %s from the environment on %s' % (var, server.id))
-    with node.remote_connection(shell=True).session as shell:
+    with node.remote_connection() as conn:
+        shell = conn.session.client.invoke_shell()
         if not shell.recv_ready():
             time.sleep(10)
         LOG.debug('Received from shell: %s' % shell.recv(4096))
