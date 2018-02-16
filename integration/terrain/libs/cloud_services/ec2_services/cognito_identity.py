4353d75583858198d6954c39327a0cb8d1720d8c
Petro Protsakh <p.protsakh@scalr.com>
2018-02-16 11:57:26 +0200

    * added AWS > Cognito Identity, Cognito User Pools, DynamoDB services validation

--- integration/terrain/libs/cloud_services/ec2_services/cognito_identity.py
+++ integration/terrain/libs/cloud_services/ec2_services/cognito_identity.py
@@ -1,3 +1,7 @@
+from botocore import exceptions as boto_exceptions
+from lettuce import world
+
+
 class CognitoIdentity(object):
     service_name = 'cognito identity'
     log_records = []
@@ -7,3 +11,20 @@ class CognitoIdentity(object):
 
     def verify(self):
         client = self.platform.get_client('cognito-identity')
+        pool_name = self.platform.get_test_name()
+        pool_id = client.create_identity_pool(IdentityPoolName=pool_name,
+                                              AllowUnauthenticatedIdentities=True)['IdentityPoolId']
+        assert any([pool for pool in client.list_identity_pools(MaxResults=10)['IdentityPools']
+                    if pool['IdentityPoolName'] == pool_name])
+        assert client.describe_identity_pool(IdentityPoolId=pool_id)['IdentityPoolName'] == pool_name
+        pool_name = pool_name + 'a'
+        client.update_identity_pool(IdentityPoolId=pool_id,
+                                    IdentityPoolName=pool_name,
+                                    AllowUnauthenticatedIdentities=False)
+        pool = client.describe_identity_pool(IdentityPoolId=pool_id)
+        assert pool['IdentityPoolName'] == pool_name
+        assert not pool['AllowUnauthenticatedIdentities']
+        assert len(client.list_identities(IdentityPoolId=pool_id, MaxResults=10)['Identities']) == 0
+        client.delete_identity_pool(IdentityPoolId=pool_id)
+        with world.assert_raises(boto_exceptions.ClientError, 'ResourceNotFoundException'):
+            client.describe_identity_pool(IdentityPoolId=pool_id)
--- integration/terrain/libs/cloud_services/ec2_services/dynamodb.py
+++ integration/terrain/libs/cloud_services/ec2_services/dynamodb.py
@@ -1,3 +1,9 @@
+import time
+
+from botocore import exceptions as boto_exceptions
+from lettuce import world
+
+
 class DynamoDb(object):
     service_name = 'dynamodb'
     log_records = []
@@ -7,3 +13,48 @@ class DynamoDb(object):
 
     def verify(self):
         client = self.platform.get_client('dynamodb')
+        table_name = self.platform.get_test_name()
+        client.create_table(TableName=table_name,
+                            AttributeDefinitions=[
+                                {
+                                    'AttributeName': 'attr1',
+                                    'AttributeType': 'S'
+                                },
+                                {
+                                    'AttributeName': 'attr2',
+                                    'AttributeType': 'S'
+                                }
+                            ],
+                            KeySchema=[
+                                {
+                                    'AttributeName': 'attr1',
+                                    'KeyType': 'HASH'
+                                },
+                                {
+                                    'AttributeName': 'attr2',
+                                    'KeyType': 'RANGE'
+                                }
+                            ],
+                            ProvisionedThroughput={
+                                'ReadCapacityUnits': 5,
+                                'WriteCapacityUnits': 5
+                            })
+        assert any([table for table in client.list_tables()['TableNames'] if table == table_name])
+        for _ in range(30):
+            table = client.describe_table(TableName=table_name)['Table']
+            if table['TableStatus'] == 'ACTIVE':
+                break
+            time.sleep(10)
+        else:
+            raise AssertionError('Operation timed out: table "%s" has not been created '
+                                 'within 5 minutes' % table_name)
+        client.delete_table(TableName=table_name)
+        for _ in range(30):
+            if not any([table for table in client.list_tables()['TableNames'] if table == table_name]):
+                break
+            time.sleep(10)
+        else:
+            raise AssertionError('Operation timed out: table "%s" has not been deleted '
+                                 'within 5 minutes' % table_name)
+        with world.assert_raises(boto_exceptions.ClientError, 'ResourceNotFoundException'):
+            client.describe_table(TableName=table_name)
