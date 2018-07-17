8f9a6e5409dcb665c5f2cec1e3bb113649b5a7b9
Theramas <n.kalmikov@scalr.com>
2018-07-17 16:35:22 +0300

    REV-404 Fix testenv delete policy

--- ui/conftest.py
+++ ui/conftest.py
@@ -4,5 +4,6 @@ import uuid
 def pytest_runtest_makereport(item, call):
     if call.when == 'call' and call.excinfo:
         item.instance.driver.save_screenshot("/vagrant/ui/%s.png" % (item.name + '-' + str(uuid.uuid1())))
+    else:
         if item.instance.container:
             item.instance.container.destroy()
