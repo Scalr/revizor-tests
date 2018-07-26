ec682da22aac94ab12ae7d9f1b5da587c8b570e2
Theramas <n.kalmikov@scalr.com>
2018-07-26 15:52:41 +0300

    REV-404 Use task argument name without '_'

--- tasks.py
+++ tasks.py
@@ -9,6 +9,12 @@ PY_VERSIONS = ['3.6.5']
 
 @task
 def grid(ctx, docs=False, port='4444'):
+    """Starts Selenium HUB, firefox node and chrome node docker containers,
+       if they're not already running.
+       Creates 'grid' docker network unless already exists.
+
+       :param str port: selenium hub port.
+    """
     if 'selenium-hub' not in ctx.run('docker ps -a').stdout:
         docker_networks = ctx.run('docker network ls')
         if 'grid' not in docker_networks.stdout:
@@ -21,10 +27,20 @@ def grid(ctx, docs=False, port='4444'):
 
 
 @task(grid)
-def webtests(ctx, test_cases='', browsers='all', processes=''):
+def webtests(ctx, testpath='', browsers='all', processes=''):
+    """Incrementally executes speicified selenium/pytest test cases with specified browsers.
+
+       :param str testpath: path to specific pytest modules or folders with tests.
+        Usage: '--testpath /vagrant/selenium/.../test.py'.
+       :param str browsers: list of browsers on which the test should be executed.
+        Usage: '--browsers firefox,chrome,...'.
+       :param str processes: number of processes for parallel testing.
+        Usage: '--processes 3'.
+    """
     browsers = ['firefox', 'chrome'] if browsers == 'all' else browsers.split(',')
     processes = ' -n %s' % processes if processes else ''
     for browser in browsers:
-        command = 'python -m pytest%s --driver Remote --host 0.0.0.0 --port 4444 --capability browserName %s %s' % (processes, browser, test_cases)
+        command = 'python -m pytest%s --driver Remote --host 0.0.0.0 --port 4444 --capability browserName %s %s' %\
+            (processes, browser, testpath)
         print(command)
         ctx.run(command)
