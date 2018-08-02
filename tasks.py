4dbdabb4aa3dbb34a44a48f1eafe95141f2be744
Theramas <n.kalmikov@scalr.com>
2018-08-02 12:08:36 +0300

    REV-404 Add seleniumdrivers task

--- tasks.py
+++ tasks.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 from invoke import task
 
 PYENV_ROOT = '/usr/local/pyenv'
@@ -21,13 +24,13 @@ def grid(ctx, docs=False, port='4444'):
             ctx.run('docker network create grid')
         grid_cmd = """
             docker run -d -p {port}:{port} --name selenium-hub selenium/hub &&
-            docker run -d -P -p 6001:5900 --link selenium-hub:hub -v /dev/shm:/dev/shm selenium/node-chrome-debug &&
-            docker run -d -P -p 5901:5900 --link selenium-hub:hub -v /dev/shm:/dev/shm selenium/node-firefox-debug""".format(port=port)
+            docker run -d --link selenium-hub:hub -v /dev/shm:/dev/shm selenium/node-chrome &&
+            docker run -d --link selenium-hub:hub -v /dev/shm:/dev/shm selenium/node-firefox""".format(port=port)
         ctx.run(grid_cmd)
 
 
-@task(grid)
-def webtests(ctx, testpath='', browsers='all', processes=''):
+@task
+def webtests(ctx, testpath='', browsers='all', processes='', localmode=''):
     """Incrementally executes speicified selenium/pytest test cases with specified browsers.
 
        :param str testpath: path to specific pytest modules or folders with tests.
@@ -36,11 +39,37 @@ def webtests(ctx, testpath='', browsers='all', processes=''):
         Usage: '--browsers firefox,chrome,...'.
        :param str processes: number of processes for parallel testing.
         Usage: '--processes 3'.
+       :param localmode str: for test runs on local machine. 
+        Usage '--localmode true'.
     """
     browsers = ['firefox', 'chrome'] if browsers == 'all' else browsers.split(',')
     processes = ' -n %s' % processes if processes else ''
     for browser in browsers:
-        command = 'python -m pytest%s --driver Remote --host 0.0.0.0 --port 4444 --capability browserName %s %s' %\
-            (processes, browser, testpath)
+        driver = browser if localmode else 'Remote'
+        command = 'python3 -m pytest%s --driver %s --host 0.0.0.0 --port 4444 --capability browserName %s %s' %\
+            (processes, driver, browser, testpath)
         print(command)
         ctx.run(command)
+
+
+@task
+def seleniumdrivers(ctx):
+    """Install geckodriver and chromedriver for local machine selenium runs.
+    """
+    if sys.platform == 'darwin':
+        urls = [
+            'https://chromedriver.storage.googleapis.com/2.41/chromedriver_mac64.zip',
+            'https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-macos.tar.gz']
+    elif sys.platform == 'linux':
+        urls = [
+            'https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip',
+            'https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz']
+    else:
+        raise NotImplementedError('Your OS is not MacOS or Linux type. You need to install chromedriver and geckodriver manually.')
+    for url in urls:
+        name = 'chromedriver' if 'chromedriver' in url else 'geckodriver'
+        ctx.run('wget %s' % url)
+        ctx.run('tar -xvzf %s*' % url.split('/')[-1])
+        ctx.run('chmod +x %s' % name)
+        ctx.run('rm -rf %s' % url.split('/')[-1])
+        ctx.run('mv {0} /usr/local/bin/{0}'.format(name))
