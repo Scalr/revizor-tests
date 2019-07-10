f006712713abd9062d34790588f8c44355f85f4a
Oleksandr Timoshyn <o.timoshyn@scalr.com>
2019-07-10 14:05:11 +0300

    fix

--- api/tests/user/test_cost_centers.py
+++ api/tests/user/test_cost_centers.py
@@ -15,13 +15,12 @@ class TestCostCentersUserScope(object):
         assert resp.json()['data'][0]['id'] == self.cost_center_id
 
     def test_cost_centers_invalid_environment_list(self, api):
-        invalid_env_Id = 1
         exc_message = "Invalid environment."
         with pytest.raises(requests.exceptions.HTTPError) as err:
             resp = api.list(
                 "/api/v1beta0/user/envId/cost-centers/",
                 params=dict(
-                envId=invalid_env_Id))
+                envId=1))
         assert err.value.response.status_code == 404
         assert exc_message in err.value.response.text
 
@@ -36,7 +35,6 @@ class TestCostCentersUserScope(object):
 
 class TestCostCentersAccountScope(object):
     account_id = "1"
-    invalid_account_id = "10"
     cost_center_Id = "3f54770e-bf1a-11e3-92c5-000feae9c516"
 
         #acc_cc_list
@@ -48,12 +46,11 @@ class TestCostCentersAccountScope(object):
         assert resp.json()['data'][0]['id']
 
     def test_cost_centers_invalid_acc_list(self, api):
-        invalid_account_id = "90"
         with pytest.raises(requests.exceptions.HTTPError) as error:
             resp = api.list(
             "/api/v1beta0/account/accountId/cost-centers/",
             params=dict(
-                accountId=self.invalid_account_id))
+                accountId=90))
         assert error.value.response.status_code == 404
 
         #acc_cc_get
