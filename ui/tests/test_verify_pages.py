1d0e6dc2f8d6028a8d9a92bbbac8864c979c026e
gigimon <oleg@scalr.com>
2019-12-16 12:59:03 +0300

    SCALRCORE-14209 fix typo

--- ui/tests/test_verify_pages.py
+++ ui/tests/test_verify_pages.py
@@ -16,13 +16,13 @@ from pages.login import LoginPage
 
 class TestPagesForErrors:
     testenv: TestEnv
-    topmenu_button: browser.SeleneElement = s('a.x-btn-scalr')
+    topmenu_button: browser.SeleneElement = s("a.x-btn-scalr")
     topmenu: browser.SeleneElement
     menu_scrolldown: browser.SeleneElement
     menu_items: browser.SeleneCollection
 
-    @pytest.fixture(autouse=True, params=['/', '/index7.html'])
-    def cleanup_cookies(self, request: FixtureRequest,  testenv: TestEnv):
+    @pytest.fixture(autouse=True, params=["/", "/index7.html"])
+    def cleanup_cookies(self, request: FixtureRequest, testenv: TestEnv):
         self.testenv = testenv
         self.base_url = request.param
         yield
@@ -31,15 +31,17 @@ class TestPagesForErrors:
 
     def assert_errors(self):
         driver = browser.driver()
-        logs = driver.get_log('browser')
+        logs = driver.get_log("browser")
         for log in logs:
-            if log['source'] == 'network':
+            if log["source"] == "network":
                 continue
-            raise AssertionError(f'Browser has an error in console: {logs}')
+            raise AssertionError(f"Browser has an error in console: {logs}")
 
     def authorize(self, username, pasword):
-        browser.open_url(f'https://{self.testenv.te_id}.test-env.scalr.com{self.base_url}')
-        s('#loading').should_not_be(visible, timeout=20)
+        browser.open_url(
+            f"https://{self.testenv.te_id}.test-env.scalr.com{self.base_url}"
+        )
+        s("#loading").should_not_be(visible, timeout=20)
         login_page = LoginPage()
         login_page.set_username(username)
         login_page.set_password(pasword)
@@ -47,7 +49,7 @@ class TestPagesForErrors:
 
     def get_menu_items_count(self, menu_selector: str):
         self.topmenu_button.click()
-        self.topmenu = s(f'div.{menu_selector}')
+        self.topmenu = s(f"div.{menu_selector}")
         self.topmenu.should_be(visible)
         menu_items_count = len(self.scalr_menu_items)
         self.topmenu_button.click()
@@ -61,53 +63,66 @@ class TestPagesForErrors:
             self.topmenu.should_be(visible)
             while not self.scalr_menu_items[i].is_displayed():
                 self.scalr_menu_scrolldown.click()
-            ss('div.x-mask').should_not_be(visible)
+            ss("div.x-mask").should_not_be(visible)
             self.scalr_menu_items[i].parent_element.hover().click()
-            components.loading_modal(consts.LoadingModalMessages.LOADING_PAGE).should_not_be(visible, timeout=10)
-            ss('div.x-mask').should_not_be(visible)
+            components.loading_modal(
+                consts.LoadingModalMessages.LOADING_PAGE
+            ).should_not_be(visible, timeout=10)
+            ss("div.x-mask").should_not_be(visible)
             self.assert_errors()
 
     @property
     def scalr_menu_scrolldown(self):
-        return self.topmenu.s('div.x-box-scroller-bottom')
+        return self.topmenu.s("div.x-box-scroller-bottom")
 
     @property
     def scalr_menu_items(self):
-        return self.topmenu.ss('div.x-menu-item[id^=menuitem] > a.x-menu-item-link-href')
+        return self.topmenu.ss(
+            "div.x-menu-item[id^=menuitem] > a.x-menu-item-link-href"
+        )
 
     def test_global_scope_pages(self):
-        self.authorize(CONF.credentials.testenv.accounts.super_admin.username,
-                       CONF.credentials.testenv.accounts.super_admin.password)
-        menu_items = self.get_menu_items_count('x-menu-scalr')
+        self.authorize(
+            CONF.credentials.testenv.accounts.super_admin.username,
+            CONF.credentials.testenv.accounts.super_admin.password,
+        )
+        menu_items = self.get_menu_items_count("x-menu-scalr")
         self.iterate_scalr_menu(menu_items)
 
     def test_account_scope_pages(self):
-        self.authorize(CONF.credentials.testenv.accounts.admin.username,
-                       CONF.credentials.testenv.accounts.admin.password)
+        self.authorize(
+            CONF.credentials.testenv.accounts.admin.username,
+            CONF.credentials.testenv.accounts.admin.password,
+        )
         # go to account scope
-        s('a.x-btn-environment').click()
-        s('div.x-menu-environment').should_be(visible)
-        s('div.x-menu-favorite-account-container').parent_element.click()
-        components.loading_modal(consts.LoadingModalMessages.LOADING_PAGE).should_be(visible).should_not_be(visible,
-                                                                                                            timeout=10)
-        ss('div.x-mask').should_not_be(visible)
+        s("a.x-btn-environment").click()
+        s("div.x-menu-environment").should_be(visible)
+        s("div.x-menu-favorite-account-container").parent_element.click()
+        components.loading_modal(consts.LoadingModalMessages.LOADING_PAGE).should_be(
+            visible
+        ).should_not_be(visible, timeout=10)
+        ss("div.x-mask").should_not_be(visible)
 
-        menu_items = self.get_menu_items_count('x-menu-account')
+        menu_items = self.get_menu_items_count("x-menu-account")
         self.iterate_scalr_menu(menu_items)
 
     def test_classic_environment_scope_pages(self):
-        self.authorize(CONF.credentials.testenv.accounts.admin.username,
-                       CONF.credentials.testenv.accounts.admin.password)
-        s('div.x-mask').should_not_be(visible)
+        self.authorize(
+            CONF.credentials.testenv.accounts.admin.username,
+            CONF.credentials.testenv.accounts.admin.password,
+        )
+        s("div.x-mask").should_not_be(visible)
 
-        menu_items = self.get_menu_items_count('x-menu-environment')
+        menu_items = self.get_menu_items_count("x-menu-environment")
         self.iterate_scalr_menu(menu_items)
 
     def test_nextgen_environment_scope_pages(self):
-        self.authorize(CONF.credentials.testenv.accounts.terraform.username,
-                       CONF.credentials.testenv.accounts.terraform.password)
+        self.authorize(
+            CONF.credentials.testenv.accounts.terraform.username,
+            CONF.credentials.testenv.accounts.terraform.password,
+        )
 
-        s('div.x-mask').should_not_be(visible)
+        s("div.x-mask").should_not_be(visible)
 
-        menu_items = self.get_menu_items_count('x-menu-environment')
+        menu_items = self.get_menu_items_count("x-menu-environment")
         self.iterate_scalr_menu(menu_items)
