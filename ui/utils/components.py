ef7f1c435570ea146e9c246c30d003ef2c4415e6
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2020-03-10 17:44:45 +0200

    SCALRCORE-14957 > AQA > UI Tests > Add Class WorkspaceDashboard (#236)

--- ui/utils/components.py
+++ ui/utils/components.py
@@ -13,13 +13,15 @@ class BaseComponent:
 
 
 class Button(BaseComponent):
-    def __init__(self, title: tp.Optional[str] = None, icon: tp.Optional[str] = None, ticon: tp.Optional[str] = None):
+    def __init__(self, title: tp.Optional[str] = None, icon: tp.Optional[str] = None, ticon: tp.Optional[str] = None, qtip: tp.Optional[str] = None):
         if title:
             self.element = s(by.xpath(f'//span[text()="{title}"]//ancestor::a'))
         elif icon:
             self.element = s(by.xpath(f'//span[contains(@class, "x-btn-icon-{icon}")]//ancestor::a'))
         elif ticon:
             self.element = s(by.xpath(f'//a[contains(@class, "x-grid-action-button-{ticon}")]'))
+        elif qtip:
+            self.element = s(by.xpath(f'//a[@data-qtip="{qtip}"]'))
         else:
             raise AssertionError('You must set one of input parameters')
 
@@ -65,14 +67,17 @@ input = Input
 
 class Combobox(BaseComponent):
     def __init__(self, label: str):
-        self.element = s(by.xpath(f'//label/span[text()="{label}"]/../parent::div'))
+        self.element = s(by.xpath(f'//label//span[text()="{label}"]/ancestor::div[1]'))
         self._bound_id = None
 
     @property
     def bound_id(self):
         if self._bound_id is None:
             self.open()
-            self._bound_id = ss('div.x-boundlist[componentid^="boundlist"]').element_by(be.visible).get(query.attribute('id'))
+            if self.element.get(query.attribute('role')):  # In ext js 7 exist
+                self._bound_id = '{}-picker'.format(self.element.get(query.attribute('id')))
+            else:
+                self._bound_id = ss('div.x-boundlist[componentid^="boundlist"]').element_by(be.visible).get(query.attribute('id'))
             self.close()
         return self._bound_id
 
@@ -126,3 +131,15 @@ class Toggle(BaseComponent):
 
 
 toggle = Toggle
+
+class SearchField(BaseComponent):
+    def __init__(self):
+        self.element = s(by.xpath("//div[text()='Search']"))
+
+    def set_value(self, value):
+        self.element.click()
+        # s(by.xpath("//div[text()='Search']/following-sibling::input")).set_value(value)
+        self.element.s(by.xpath("following-sibling::input")).set_value(value)
+
+
+search = SearchField
