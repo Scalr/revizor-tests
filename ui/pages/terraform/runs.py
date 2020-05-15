57f0d1ddbc71d2cd8ab71e13131eaa73de83c1c7
olya-stasyuk <olya.stasyuk@outlook.com>
2020-05-15 13:28:27 +0300

    fix feature/SCALRCORE-14156

--- ui/pages/terraform/runs.py
+++ ui/pages/terraform/runs.py
@@ -16,11 +16,11 @@ class RunStep:
 
     @property
     def title(self):
-        return self.element.s('td.x-title')
+        return self.element.s("td.x-title")
 
     @property
     def status(self):
-        return self.element.s('div.x-status')
+        return self.element.s("div.x-status")
 
     def activate(self):
         self.element.click()
@@ -30,60 +30,62 @@ class RunStep:
 class RunDashboard(TfBasePage):
     @staticmethod
     def wait_page_loading():
-        loading_modal(consts.LoadingModalMessages.LOADING_PAGE).should(be.not_.visible, timeout=20)
-        s('div.x-grid-buffered-loader-mask').should(be.not_.visible, timeout=10)
-        s('[id^=breadcrumbs]').should(have.text('run-'))
+        loading_modal(consts.LoadingModalMessages.LOADING_PAGE).should(
+            be.not_.visible, timeout=20
+        )
+        s("div.x-grid-buffered-loader-mask").should(be.not_.visible, timeout=10)
+        s("[id^=breadcrumbs]").should(have.text("run-"))
 
     def __init__(self):
-        self.element = ss('div.x-abs-layout-item').filtered_by(be.visible)[0]
+        self.element = ss("div.x-abs-layout-item").filtered_by(be.visible)[0]
 
     @property
     def date(self) -> SeleneElement:
-        return self.element.ss('fieldset')[0].ss('div.x-field')[0]
+        return self.element.ss("fieldset")[0].ss("div.x-field")[0]
 
     @property
     def status(self) -> SeleneElement:
-        return self.element.ss('fieldset')[0].ss('div.x-field')[1]
+        return self.element.ss("fieldset")[0].ss("div.x-field")[1]
 
     @property
     def run_id(self) -> SeleneElement:
-        return self.element.ss('fieldset')[1].ss('div.x-field')[0]
+        return self.element.ss("fieldset")[1].ss("div.x-field")[0]
 
     @property
     def commit(self) -> SeleneElement:
-        return self.element.ss('fieldset')[1].ss('div.x-field')[1]
+        return self.element.ss("fieldset")[1].ss("div.x-field")[1]
 
     @property
     def triggered_by(self) -> SeleneElement:
-        return self.element.ss('fieldset')[2].ss('div.x-field')[0]
+        return self.element.ss("fieldset")[2].ss("div.x-field")[0]
 
     @property
     def triggered_from(self) -> SeleneElement:
-        return self.element.ss('fieldset')[2].ss('div.x-field')[1]
+        return self.element.ss("fieldset")[2].ss("div.x-field")[1]
 
     @property
     def workspace(self) -> SeleneElement:
-        return self.element.ss('fieldset')[3].ss('div.x-field')[0]
+        return self.element.ss("fieldset")[3].ss("div.x-field")[0]
 
     @property
     def repository(self) -> SeleneElement:
-        return self.element.ss('fieldset')[3].ss('div.x-field')[1]
+        return self.element.ss("fieldset")[3].ss("div.x-field")[1]
 
     @property
     def approve_button(self) -> button:
-        return button('Approve', parent=self.element)
+        return button("Approve", parent=self.element)
 
     @property
     def decline_button(self) -> button:
-        return button('Decline', parent=self.element)
+        return button("Decline", parent=self.element)
 
     @property
     def steps(self) -> [RunStep]:
-        return [RunStep(e) for e in self.element.ss('div.x-dataview-tab')]
+        return [RunStep(e) for e in self.element.ss("div.x-dataview-tab")]
 
     @property
     def console(self) -> SeleneElement:
-        return s('div.x-logviewer-log')
+        return s("div.x-logviewer-log")
 
     @property
     def confirm_approve(self) -> button:
@@ -92,11 +94,10 @@ class RunDashboard(TfBasePage):
     @property
     def confirm_decline(self) -> button:
         return s(by.xpath("//span[text()='Yes']/ancestor::span"))
-    
+
     @property
     def open_ws_runs(self) -> browser.element:
         return s(by.xpath("//a[text()='Runs']"))
-      
 
 
 class WorkspaceRunLine:
@@ -105,73 +106,80 @@ class WorkspaceRunLine:
 
     @property
     def icon_status(self) -> SeleneElement:
-        return self.element.ss('td')[0]
+        return self.element.ss("td")[0]
 
     @property
     def date(self) -> SeleneElement:
-        return self.element.ss('td')[1].ss('div.x-grid-dataview-cell')[0]
-    
+        return self.element.ss("td")[1].ss("div.x-grid-dataview-cell")[0]
+
     @property
     def carrent(self) -> SeleneElement:
-        return self.element.ss('td')[1].s('span.x-tag-label-blue')
+        return self.element.ss("td")[1].s("span.x-tag-label-blue")
 
     @property
     def status(self) -> SeleneElement:
-        return self.element.ss('td')[1].ss('div.x-grid-dataview-cell')[1]
+        return self.element.ss("td")[1].ss("div.x-grid-dataview-cell")[1]
 
     @property
     def run_id(self) -> SeleneElement:
-        return self.element.ss('td')[2].s('a')[0]
+        return self.element.ss("td")[2].s("a")[0]
 
     @property
     def copy_run(self) -> SeleneElement:
-        return self.element.ss('td')[2].s('img.x-icon-copy')
+        return self.element.ss("td")[2].s("img.x-icon-copy")
 
     @property
     def commit(self) -> SeleneElement:
-        return self.element.ss('td')[2].s('a')[1]
+        return self.element.ss("td")[2].s("a")[1]
 
     @property
     def triggered_by(self) -> SeleneElement:
-        return self.element.ss('td')[3].ss('div.x-grid-dataview-cell')[0]
+        return self.element.ss("td")[3].ss("div.x-grid-dataview-cell")[0]
 
     @property
     def triggered_from(self) -> SeleneElement:
-        return self.element.ss('td')[3].ss('div.x-grid-dataview-cell')[1]
+        return self.element.ss("td")[3].ss("div.x-grid-dataview-cell")[1]
 
     @property
     def run_details_button(self) -> button:
-        return button(qtip='Run Details', parent=self.element)
-        
+        return button(qtip="Run Details", parent=self.element)
+
+
 class QueueNewRunModal:
     @staticmethod
     def wait_page_loading():
-        s('div#loading').should(be.not_.visible, timeout=20)
+        s("div#loading").should(be.not_.visible, timeout=20)
         s(by.xpath("//span[text()='Cancel']/ancestor::span")).should(be.visible)
 
     @property
-    def queue_button(self): 
+    def queue_button(self) -> button:
         return s(by.xpath("//span[text()='Queue Run']/ancestor::span"))
 
     @property
-    def cancel_queue_button(self):
+    def cancel_queue_button(self) -> button:
         return s(by.xpath("//span[text()='Cancel']/ancestor::span"))
 
 
 class WorkspaceRunsPage(TfBasePage):
     @staticmethod
     def wait_page_loading():
-        loading_modal('Loading...').should(be.not_.visible, timeout=10)
-        s(by.xpath("//span[text()='Queue run']/ancestor::span")).should(be.visible, timeout=20)
+        loading_modal("Loading...").should(be.not_.visible, timeout=10)
+        s(by.xpath("//span[text()='Queue run']/ancestor::span")).should(
+            be.visible, timeout=20
+        )
 
     @property
     def queue_run(self) -> button:
-        return button(title='Queue run')
+        return button(title="Queue run")
 
     @property
-    def refresh(self):
-        return s(by.xpath("//div[starts-with(@id, 'workspacedashboardruns')]//a[@data-qtip='Refresh']"))
+    def refresh(self) -> button:
+        return s(
+            by.xpath(
+                "//div[starts-with(@id, 'workspacedashboardruns')]//a[@data-qtip='Refresh']"
+            )
+        )
 
     @property
-    def workspace_run(self) -> [WorkspaceRunLine]:
-        return [WorkspaceRunLine(p) for p in ss('tr.x-grid-row') if p.is_displayed()]
+    def workspace_runs(self) -> [WorkspaceRunLine]:
+        return [WorkspaceRunLine(p) for p in ss("tr.x-grid-row") if p.is_displayed()]
